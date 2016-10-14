cbb56bc47eef4c46acece85c4d5a3eb56d7412a2
James <james@jarofgreen.co.uk>
2016-10-14 14:28:40 +0100

    Twitter formatted "Twitter:" in notes

--- AWSLambda/tests.py
+++ AWSLambda/tests.py
@@ -30,6 +30,21 @@ class TestTwitter(unittest.TestCase):
         self.assertEqual(person.getTwitter(), 'LizBarkerLords')
 
 
+    def test_2(self):
+        person = ModelBase()
+
+        address1 = ModelAddressBase()
+        address1.address1 = 'My House'
+        address1.address2 = 'My Street'
+        person.addresses.append(address1)
+
+        address2 = ModelAddressBase()
+        address2.note = 'Twitter: TariqBt1'
+        person.addresses.append(address2)
+
+        self.assertEqual(person.getTwitter(), 'TariqBt1')
+
+
 
 if __name__ == '__main__':
     unittest.main()
